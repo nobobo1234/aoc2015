ae4b643570835d57b763e41d99ee404db3f8e3d3
Noah van Boven <noah.van.boven@student.uva.nl>
2021-11-15 21:07:47 +0100

    Day 1 part 1 finished

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,14 @@ mod day01;
 
 fn main() {
     println!("Running day 1:");
-    println!("Solution to part 1: {}", day01::part1());
+
+    let part1 = match day01::part1() {
+        Ok(result) => result,
+        Err(e) => {
+            println!("Error: {}", e);
+            -1
+        }
+    };
+
+    println!("Solution to part 1: {}", part1);
 }
